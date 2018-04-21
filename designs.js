96fd099923655a1a43c829912c66f55c854a8df2
Tariq-Kakar <trqkkr@gmail.com>
2018-04-20 19:36:08 -0700

    Add functions to clear oldGrid and create makeGrid

--- designs.js
+++ designs.js
@@ -5,10 +5,24 @@ const size = document.querySelector('#sizePicker');
 const height = document.querySelector('#inputHeight');
 const width = document.querySelector('#inputWeight');
 const canvas = document.querySelector('#pixelCanvas');
-// When size is submitted by the user, call makeGrid()
-
+// clear old grid
+function clearOldGrid() {
+    canvas.innerHTML = '';
+}
+//make new grid
 function makeGrid() {
-
-// Your code goes here!
+    let tr, td;
+    //create table
+    //for loop runs inside, out
+    //so create td, append it to tr
+    //create tr, append it to canvas
+    for (let i = 0; i < height.value; i++) {
+        tr = document.createElement('tr');
+        canvas.appendChild(tr);
+        for (let j = 0; j < width.value; j++) {
+          td = document.createElement('td');
+          tr.appendChild(td);
+        }
+    }
 
 }
