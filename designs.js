d28e4280d2f62ad20f0847d958192bcf5d3aa7ab
Tariq-Kakar <trqkkr@gmail.com>
2018-04-20 18:22:05 -0700

    Store Id's in variables
    The color and size inputs are added into their respective variables

--- designs.js
+++ designs.js
@@ -1,6 +1,10 @@
 // Select color input
+const color = document.querySelector('#colorPicker');
 // Select size input
-
+const size = document.querySelector('#sizePicker');
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWeight');
+const canvas = document.querySelector('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
